c84f1152b7091c1ec337bb4cdbaab0901442565e
William Walker <w_walker@icloud.com>
2026-02-26 18:00:17 -0500

    fix: revert ESM bundling in lieu of CJS

--- index.mjs
+++
@@ -1,7 +0,0 @@
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-const hmmfetch = require('./index.js');
-
-export const { generateHeaders } = hmmfetch;
-export default hmmfetch;
